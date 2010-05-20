6ec8122afa2314fe13be47feea8ea04577a7b5b5
Andy Parkins <andyparkins@gmail.com>
2010-05-20 15:39:18 +0100

    Add GPL license file and add license to top of source

--- gitblogger.py
+++ gitblogger.py
@@ -10,6 +10,19 @@
 # Legal
 #    Copyright 2010  Andy Parkins
 #
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # Notes
 #   http://code.google.com/apis/blogger/docs/2.0/developers_guide_protocol.html
 #
